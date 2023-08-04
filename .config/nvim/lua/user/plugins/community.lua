f16c39607e8a6b7553fe904d2444ab02dc1794bd
Colin Thatcher <colinthatcher0@gmail.com>
2023-08-04 13:08:15 -0400

    small tmux updates

--- .config/nvim/lua/user/plugins/community.lua
+++ .config/nvim/lua/user/plugins/community.lua
@@ -6,17 +6,21 @@ return {
 
   { import = "astrocommunity.colorscheme.catppuccin" },
   { import = "astrocommunity.motion.harpoon" },
-  { "ThePrimeagen/harpoon", keys = {
-    { "<leader>hh", function() require("harpoon.mark").add_file() end, desc = "Add file" },
-    { "<leader>hv", function() require("harpoon.ui").toggle_quick_menu() end, desc = "Toggle quick menu" },
-    { "<leader>ha", function() require("harpoon.ui").nav_file(1) end, desc = "Nav file 1" },
-    { "<leader>hr", function() require("harpoon.ui").nav_file(2) end, desc = "Nav file 2" },
-    { "<leader>hs", function() require("harpoon.ui").nav_file(3) end, desc = "Nav file 3" },
-    { "<leader>ht", function() require("harpoon.ui").nav_file(4) end, desc = "Nav file 4" },
-  }},
+  {
+    "ThePrimeagen/harpoon",
+    keys = {
+      { "<leader>hh", function() require("harpoon.mark").add_file() end, desc = "Add file" },
+      { "<leader>hv", function() require("harpoon.ui").toggle_quick_menu() end, desc = "Toggle quick menu" },
+      { "<leader>ha", function() require("harpoon.ui").nav_file(1) end, desc = "Nav file 1" },
+      { "<leader>hr", function() require("harpoon.ui").nav_file(2) end, desc = "Nav file 2" },
+      { "<leader>hs", function() require("harpoon.ui").nav_file(3) end, desc = "Nav file 3" },
+      { "<leader>ht", function() require("harpoon.ui").nav_file(4) end, desc = "Nav file 4" },
+    },
+  },
 
   -- language packs
   { import = "astrocommunity.pack.python" },
   { import = "astrocommunity.pack.go" },
   { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.pack.rust" },
 }
