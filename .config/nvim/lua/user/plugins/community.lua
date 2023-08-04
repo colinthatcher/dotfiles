0d2e88039d6c54ddc2c7ecc045fa3e5d51ae5b18
Colin Thatcher <colinthatcher0@gmail.com>
2023-08-04 08:11:23 -0400

    added astronvim packs

--- .config/nvim/lua/user/plugins/community.lua
+++ .config/nvim/lua/user/plugins/community.lua
@@ -4,6 +4,19 @@ return {
   -- example of imporing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
-  -- { import = "astrocommunity.colorscheme.catppuccin" },
-  -- { import = "astrocommunity.completion.copilot-lua-cmp" },
+  { import = "astrocommunity.colorscheme.catppuccin" },
+  { import = "astrocommunity.motion.harpoon" },
+  { "ThePrimeagen/harpoon", keys = {
+    { "<leader>hh", function() require("harpoon.mark").add_file() end, desc = "Add file" },
+    { "<leader>hv", function() require("harpoon.ui").toggle_quick_menu() end, desc = "Toggle quick menu" },
+    { "<leader>ha", function() require("harpoon.ui").nav_file(1) end, desc = "Nav file 1" },
+    { "<leader>hr", function() require("harpoon.ui").nav_file(2) end, desc = "Nav file 2" },
+    { "<leader>hs", function() require("harpoon.ui").nav_file(3) end, desc = "Nav file 3" },
+    { "<leader>ht", function() require("harpoon.ui").nav_file(4) end, desc = "Nav file 4" },
+  }},
+
+  -- language packs
+  { import = "astrocommunity.pack.python" },
+  { import = "astrocommunity.pack.go" },
+  { import = "astrocommunity.pack.lua" },
 }
